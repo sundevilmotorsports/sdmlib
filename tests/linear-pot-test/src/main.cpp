b15d2779b489c362f33a7c648bf6f812956c4865
Meshach Samuel <83641520+MESHach12345@users.noreply.github.com>
2022-10-01 11:35:11 -0700

    Final Changes To LinearPotTest

--- tests/linear-pot-test/src/main.cpp
+++ tests/linear-pot-test/src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
+#include <vector>
 
 // pin on the teensy
-const int pin = A5;
+const int pin = A6;
 
 // multiplier
 float scale = 0.00244379;
@@ -20,26 +21,38 @@ int getRaw(){
 
 // zero'd location of the potentiometer
 float zeroValue = 0.0;
+float sum=0;
 
 // zero's out the potentiometer
 // i.e., set the zero'd location
 void calibrate(int samples){
-
+    
+    using std::vector;
+    static vector<float> values(samples);
+    for (int i = 0; i < samples; i++){
+        values.insert(values.begin(), get());
+        sum = sum+get();
+    }
+    zeroValue = sum/samples;
 }
+
 
 // returns the displacement of the potentiometer from its zero'd position
 float getDisplacement(){
-    float displacement = 0.0;
-
+    float displacement = get();
+    displacement = zeroValue - displacement;
     return displacement;
 }
 
 
 void setup() {
     // put your setup code here, to run once:
+    Serial.begin(9600);
+    calibrate(500);
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-
+    getDisplacement();
+    Serial.println(getDisplacement());
 }
