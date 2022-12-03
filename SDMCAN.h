328871e9b333c61b4ca9e562c15654f755b23c08
JackLusenhop <jack.lusenhop@gmail.com>
2022-12-03 11:57:30 -0700

    finished send and added recieve functions
    Co-authored-by: Nivedh Mudiam <N43527@users.noreply.github.com>

--- SDMCAN.h
+++ SDMCAN.h
@@ -2,18 +2,26 @@
 #include <FlexCAN_T4.h>
 
 class SDMCAN {
-    public: 
     
+    
+    public: 
     // message id 0x360
     enum CANData {
         AccelData,
         RotationData,
+        // air speed, yaw angle
         YawProbeData,
-        PressueData,
-        FLData,
-        FRData
+        // front brake pressure, rear brake pressure
+        AnglePressureHeightData,
+        // Damper Travel, Wheel Speed, Brake Rotor Temperature, Push Rod Load
+        // for both
+        FrontLeftData,
+        FrontRigtData
     };
+    FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
     
+    SDMCAN();   
+    SDMCAN(FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> CAN);   
     void send(CANData type, std::vector<float> data);
-    void send(CANData type, std::vector<int> data);
+    std::vector<float> recieve(CAN_message_t message);
 };
